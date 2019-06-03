626a286fa97c322fbf79c037f4d9e33a8c566185
Matthew 'siD' Van der Bijl <matthewvdbijl@gmail.com>
2019-06-03 21:48:24 +0200

    Testing new models

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AngularFirestore } from '@angular/fire/firestore';
+import { List, LIST_TABLE } from '../models/list.model';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +10,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  lists: any;
+
+  // userUid: string; // FIXME: load when needed in method
+
+  constructor(private afs: AngularFirestore) { }
 
   ngOnInit() {
+    this.lists = this.afs.collection(LIST_TABLE).valueChanges();
+
+    // this.auth.user$.subscribe(user => {
+    //   this.userUid = user.uid;
+    //   console.log('User Uid loaded:' + this.userUid);
+    // });
+  }
+
+  add() {
+    const item = window.prompt('enter name');
+    if (!item) {
+      return;
+    }
+    this.createList(item, 'hello world');
+  }
+
+  /**
+   * Insert new list in to database. Generates and attached primary key.
+   * 
+   * @param name name of the list
+   * @param description list descriptions
+   */
+  createList(name: string, description: string): void {
+    const uid = this.afs.createId();
+    this.afs.collection<List>(LIST_TABLE).doc(uid).set(
+      {
+        uid,
+        name,
+        description
+      }
+    );
   }
 
 }
--- src/app/list-tree/list-tree.component.ts
+++ src/app/list-tree/list-tree.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-
-import { ListItem } from '../models/list-item';
+import { Component, OnInit, Input } from '@angular/core';
+import { List } from '../models/List.model';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { ListItem, LIST_ITEMS_TABLE } from '../models/list-item.model';
 
 @Component({
   selector: 'app-list-tree',
@@ -9,26 +10,32 @@ import { ListItem } from '../models/list-item';
 })
 export class ItemTreeComponent implements OnInit {
 
-  data: ListItem[] = [
-    { item: 'a', isChecked: false },
-    { item: 'b', isChecked: false },
-    { item: 'c', isChecked: true },
-    { item: 'd', isChecked: false },
-    { item: 'e', isChecked: false },
-    { item: 'f', isChecked: false },
-    { item: 'g', isChecked: true },
-    { item: 'h', isChecked: false },
-  ];
+  @Input() list: List;
 
-  constructor() {
+  items: any;
 
-  }
+  constructor(private afs: AngularFirestore) { }
 
   ngOnInit() {
+    this.items = this.afs.collection(LIST_ITEMS_TABLE,
+      ref => ref.where('list', '==', this.list.uid)).valueChanges();
   }
 
-  onClick(node: ListItem) {
-    node.isChecked = !node.isChecked;
-    // console.log(node);
+  add() {
+    const item = window.prompt('enter name');
+    if (!item) {
+      return;
+    }
+
+    const uid = this.afs.createId();
+    const list = this.list.uid;
+
+    this.afs.collection<ListItem>(LIST_ITEMS_TABLE).doc(uid).set(
+      {
+        uid,
+        list,
+        item
+      }
+    );
   }
 }
