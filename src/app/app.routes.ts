6240112f127c7fa45698eeb928308e5c2153051f
Matthew 'siD' Van der Bijl <matthewvdbijl@gmail.com>
2019-06-09 20:06:48 +0200

    Add route to User page
    Routes to ngx-auth-firebaseui-user component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 
 import { AuthGuard } from './services/auth.guard';
 
+import { UserComponent } from 'ngx-auth-firebaseui';
+
 import { HomeComponent } from './pages/home/home.component';
 import { SigninComponent } from './pages/signin/signin.component';
 
@@ -9,5 +11,6 @@ export const ROUTES: Routes = [
     { path: '', redirectTo: '/signin', pathMatch: 'full' },
     { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
     { path: 'signin', component: SigninComponent },
+    { path: 'user', component: UserComponent, canActivate: [AuthGuard] }, // ngx-auth-firebaseui-user
     // { path: '**', component: PageNotFoundComponent }
 ];
