28e09706ad8690e55161027df318004f533f6ec0
Matthew 'siD' Van der Bijl <matthewvdbijl@gmail.com>
2019-06-15 18:03:23 +0200

    Readd Page Not Found Component
    4mood_bad4

--- src/app/routing/app.routes.ts
+++ src/app/routing/app.routes.ts
@@ -4,11 +4,12 @@ import { UserComponent, LoggedInGuard } from 'ngx-auth-firebaseui';
 
 import { HomeComponent } from './home/home.component';
 import { SigninComponent } from './signin/signin.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const ROUTES: Routes = [
     { path: '', redirectTo: '/signin', pathMatch: 'full' },
     { path: 'home', component: HomeComponent, canActivate: [LoggedInGuard] },
     { path: 'signin', component: SigninComponent },
     { path: 'user', component: UserComponent, canActivate: [LoggedInGuard] }, // ngx-auth-firebaseui-user
-    // { path: '**', component: PageNotFoundComponent }
+    { path: '**', component: PageNotFoundComponent }
 ];
